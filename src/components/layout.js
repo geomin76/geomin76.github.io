532fecc8a65405b9574c3df98558787cbf6ce9d5
geomin76 <geom@vt.edu>
2020-10-18 23:33:59 -0400

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,110 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Layout = props => {
+  const { title, children } = props
+  const [toggleNav, setToggleNav] = React.useState(false)
+  return (
+    <div className={`site-wrapper ${toggleNav ? `site-head-open` : ``}`}>
+      <header className="site-head">
+        <div className="site-head-container">
+          <a
+            className="nav-burger"
+            href={`#`}
+            onClick={() => setToggleNav(!toggleNav)}
+          >
+            <div
+              className="hamburger hamburger--collapse"
+              aria-label="Menu"
+              role="button"
+              aria-controls="navigation"
+            >
+              <div className="hamburger-box">
+                <div className="hamburger-inner" />
+              </div>
+            </div>
+          </a>
+          <nav id="swup" className="site-head-left">
+            <ul className="nav" role="menu">
+              {/* <li className="nav-home nav-current" role="menuitem">
+                <Link to={`/`}>Home</Link>
+              </li> */}
+              <li className="nav-projects" role="menuitem">
+                <Link to={`/projects`}>Projects</Link>
+              </li>
+              <li className="nav-travel" role="menuitem">
+                <Link to={`/travel`}>Travels</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="site-head-center">
+            <Link className="site-head-logo" to={`/`}>
+              {title}
+            </Link>
+          </div>
+
+          <nav id="swup" className="site-head-right">
+            <ul className="nav" role="menu">
+              {/* <li className="nav-home nav-current" role="menuitem">
+                <Link to={`/`}>Home</Link>
+              </li> */}
+              <li className="nav-photos" role="menuitem">
+                <Link to={`/photos`}>Photography</Link>
+              </li>
+              <li className="nav-about" role="menuitem">
+                <Link to={`/about`}>About</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* <div className="site-head-right">
+            <div className="social-links">
+              <a
+                href="https://www.facebook.com"
+                title="Facebook"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Facebook
+              </a>
+              <a
+                href="https://twitter.com"
+                title="Twitter"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+              <Link
+                to={`/rss.xml`}
+                title="RSS"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                RSS
+              </Link>
+            </div>
+          </div> */}
+        </div>
+      </header>
+      <main id="site-main" className="site-main">
+        <div id="swup" className="transition-fade">
+          {children}
+        </div>
+      </main>
+      <footer className="site-foot">
+        &copy; {new Date().getFullYear()} <Link to={`/`}>{title}</Link> &mdash;
+        Built with{" "}
+        <a
+          href="https://gatsbyjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gatsby
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
