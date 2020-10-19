aac0eec4a8136e37dc77744251ae28d691ac2ccd
geomin76 <geom@vt.edu>
2020-10-19 00:17:34 -0400

    Changed icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ const BlogIndex = ({ data }, location) => {
   return (
     <Layout title={siteTitle}>
       <SEO
-        title="All posts"
+        title="Geo Min"
         keywords={[`blog`, `gatsby`, `javascript`, `react`]}
       />
       {/* <Bio /> */}
