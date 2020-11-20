71adb0ebf38d854f6023db8bb729c2bc7ca3fef2
geomin76 <geom@vt.edu>
2020-11-19 23:20:50 -0500

    Updating api keys

--- src/components/googleMaps.js
+++ src/components/googleMaps.js
@@ -5,7 +5,7 @@ import "../utils/css/screen.css"
 
 const GoogleMaps = ({ latitude, longitude }) => {
   async function getData() {
-    const response = await fetch(process.env.GATSBY_QUERY_URL, {
+    const response = await fetch(`${process.env.GATSBY_QUERY_URL}`, {
       method: "GET",
       mode: "cors",
     })
@@ -25,11 +25,11 @@ const GoogleMaps = ({ latitude, longitude }) => {
 
   return (
     <div
-      class="google-maps-transition"
+      className="google-maps-transition"
       style={{ height: "100vh", width: "100%" }}
     >
       <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.GATSBY_API_KEY }}
+        bootstrapURLKeys={{ key: `${process.env.GATSBY_API_KEY}` }}
         defaultCenter={{ lat: 40.757, lng: -73.99 }}
         defaultZoom={1}
         options={{ gestureHandling: "greedy" }}
