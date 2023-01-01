9a9480ba0f5e837b3584f1ae87569481d87ff923
geomin76 <geom@vt.edu>
2022-12-31 20:52:43 -0500

    adding notes

--- src/pages/grad-guide.js
+++ src/pages/grad-guide.js
@@ -7,14 +7,28 @@ import SEO from "../components/seo"
 import "../utils/normalize.css"
 import "../utils/css/screen.css"
 
-const GradGuide = ({ data }, location) => {
+import {
+  LazyLoadImage,
+  trackWindowScroll,
+} from "react-lazy-load-image-component"
+
+const GradGuide = ({ data, scrollPosition }, location) => {
   const siteTitle = data.site.siteMetadata.title
 
   return (
     <Layout title={siteTitle}>
       <SEO
-        title="Travels"
-        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        title="Grad Guide"
+        keywords={[
+          `grad`,
+          `graduation`,
+          `guide`,
+          `photos`,
+          `poses`,
+          `tassel`,
+          `photo`,
+          `pictures`,
+        ]}
       />
 
       <article className="post-content page-template no-image">
@@ -38,8 +52,8 @@ const GradGuide = ({ data }, location) => {
             </p>
           </p>
         </div>
-      </article>
-      <article className="post-content page-template no-image">
+        <br />
+        <br />
         <div className="post-content-body">
           <p style={{ textAlign: "left", fontSize: "16px" }}>
             Congrats!!!! You're about to graduate 👩‍🎓, and it's time to do grad
@@ -53,18 +67,108 @@ const GradGuide = ({ data }, location) => {
             ideas for you 🦫
           </p>
         </div>
+
         <div className="post-content-body">
           <h2 style={{ textAlign: "left" }} id="wear">
             How to wear your grad items!
           </h2>
-          <p style={{ textAlign: "left", fontSize: "16px" }}>info</p>
+          <p style={{ textAlign: "left", fontSize: "14px" }}>
+            To note: people generally hate wearing cap/gown in grad photos
+            (because it looks bad and the cap is a{" "}
+            <a
+              href="https://en.wikipedia.org/wiki/Square_academic_cap"
+              target="_blank"
+            >
+              strange square item
+            </a>{" "}
+            on your head), but it is GRADUATION so make sure to get at least
+            some cap/gown photos for the yourself and family!
+          </p>
+          <p style={{ textAlign: "left", fontSize: "14px" }}>
+            You can also do a combination of just gown, just cap and cap + gown
+            for photos if you hate the whole ensemble
+          </p>
+
+          <p style={{ textAlign: "left", fontSize: "18px" }}>
+            1. Your graduation cap
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <p style={{ textAlign: "left", fontSize: "14px" }}>
+                &nbsp;&nbsp; Wear your graduation cap like so:
+              </p>
+            </div>
+            <div className="col-6">
+              <LazyLoadImage
+                effect="blur"
+                scrollPosition={scrollPosition}
+                src="https://senior-pics.s3.amazonaws.com/danyaimg.jpeg"
+              />
+            </div>
+            <br />
+            <div className="col-6">
+              <p style={{ textAlign: "left", fontSize: "14px" }}>
+                &nbsp;&nbsp; Do NOT wear it like this:
+              </p>
+            </div>
+            <div className="col-6">
+              <LazyLoadImage
+                effect="blur"
+                scrollPosition={scrollPosition}
+                src="https://westwoodhorizon.com/wp-content/uploads/2022/05/Screen-Shot-2022-05-14-at-9.59.14-PM.png"
+              />
+            </div>
+          </div>
+          <br />
+          <p style={{ textAlign: "left", fontSize: "14px" }}>
+            &nbsp;&nbsp;Tassels are worn on the right side, and moved to the
+            left after graduation (to be ~ proper ~)
+          </p>
+          <p style={{ textAlign: "left", fontSize: "18px" }}>
+            2. Your graduation gown
+          </p>
+          <p style={{ textAlign: "left", fontSize: "14px" }}>
+            Wearing the gown is pretty straightforward, but there are some
+            things to note of course!
+          </p>
+          <div className="row">
+            <div className="col-6">
+              <p style={{ textAlign: "left", fontSize: "14px" }}>
+                Typically, people will want to open their gowns like this in
+                order to show off their dress/button down/suit/etc like so:
+              </p>
+            </div>
+            <div className="col-6">
+              <LazyLoadImage
+                effect="blur"
+                scrollPosition={scrollPosition}
+                src="https://senior-pics.s3.amazonaws.com/20191025-IMG_9948.jpg"
+              />
+            </div>
+            <div className="col-6">
+              <p style={{ textAlign: "left", fontSize: "14px" }}>
+                Also, there is nothing wrong with closing it up! It's all up to
+                you, but always remember you can do alllllll the poses and
+                variations of clothes you want
+              </p>
+            </div>
+            <div className="col-6">
+              <LazyLoadImage
+                effect="blur"
+                scrollPosition={scrollPosition}
+                src="https://senior-pics.s3.amazonaws.com/20200516-IMG_0678.jpg"
+              />
+            </div>
+          </div>
         </div>
+
         <div className="post-content-body">
           <h2 style={{ textAlign: "left" }} id="poses">
             Poses!
           </h2>
           <p style={{ textAlign: "left", fontSize: "16px" }}>info</p>
         </div>
+
         <div className="post-content-body">
           <h2 style={{ textAlign: "left" }} id="groupposes">
             Group poses!
